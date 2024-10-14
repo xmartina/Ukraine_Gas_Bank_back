d4ef71ae005dd231b69f27bfd2eea08ebfdf75a4
xmartina <martinsprince126@gmail.com>
2024-10-14 17:31:32 +0100

    d

--- login.php
+++ login.php
@@ -115,7 +115,7 @@ toast_alert("error","Invalid login details");
                         <div class="form">
 
                             <div id="username-field" class="field-wrapper input">
-                                <label for="username">Account ID</label>
+                                <label for="username">Account Username</label>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                                 <input id="username" name="acct_username" type="text" class="form-control" placeholder="Account Username">
                             </div>
