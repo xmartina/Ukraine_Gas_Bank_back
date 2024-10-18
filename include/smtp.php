298b8af95d3bd1963890faf66d5947fbfaa2d68a
xmartina <martinsprince126@gmail.com>
2024-10-18 14:59:23 +0100

    d

--- include/smtp.php
+++ include/smtp.php
@@ -13,18 +13,18 @@ class message{
         //SMTP Settings
         //$mail->isSMTP();
         $mail->isMail();
-        $mail->Host = "mail.ecoequitybank.com"; // Change Email Host
+        $mail->Host = "mail.ukrgb.online"; // Change Email Host
         $mail->SMTPAuth = true;
-        $mail->Username = "info@ecoequitybank.com"; // Change Email Address
-        $mail->Password = '+Ecoequitybank123'; // Change Email Password
+        $mail->Username = "info@ukrgb.online"; // Change Email Address
+        $mail->Password = '@SECukrgb1'; // Change Email Password
         $mail->Port = 587; //587
         $mail->SMTPSecure = "ssl"; //tls
 
         //Email Settings
         $mail->isHTML(true);
-        $mail->setFrom('info@ecoequitybank.com','Eco Equity Bank'); // Change
+        $mail->setFrom('info@ukrgb.online','Ukraine Gas Bank'); // Change
         $mail->addAddress($email);
-        $mail->AddReplyTo("info@ecoequitybank.com", "Eco Equity Bank"); // Change
+        $mail->AddReplyTo("info@ukrgb.online", "Ukraine Gas Bank"); // Change
         $mail->Subject = $subject;
         $mail->MsgHTML($message);
         $mail->Send();
